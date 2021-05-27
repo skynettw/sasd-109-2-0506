bd4d89083bc290276e2edb44e0a33a72902f5102
Richard <skynet.tw@gmail.com>
2021-05-27 16:29:55 +0800

    add a table to database

--- mysite/models.py
+++ mysite/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.utils import timezone
+
+class MyList(models.Model):
+	name = models.CharField(max_length=100)
+	created = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.name
 
 class TVList(models.Model):
-    title = models.CharField(max_length=100)
-    vid = models.CharField(max_length=20)
+	title = models.CharField(max_length=100)
+	vid = models.CharField(max_length=20)
+	mylist = models.ForeignKey(MyList, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
+
